2662b6bf270e9793d20b8c30271f18a9cfc4e44c
Filip Graz <filipkrk1989@gmail.com>
2020-07-08 16:02:54 +0200

    js changes

--- js/script.js
+++ js/script.js
@@ -1,104 +1,84 @@
-/* 
- tablica obiektów 2 obiektu ponieważ możemy sprawdzić jak przykłądowe 2 zadania bedą wyglądały - przykład
- 
- content treść zadania, done - boolean frua albo false
- */
-
 {
-  const tasks = [
-    {
-      content: "kupić bilety na samolot",
-      done: false,
-    },
-    {
-      content: "uczyc się Frontendu",
-      done: true,
-    },
-  ];
-
-  /*proste funkcje które robią jedną rzecz z tymi zadaniami*/
-
-  const addNewTask = (newTaskContent) => {
-    /*addNewTask dodaje nowe zadanie i przyjmuje 1 argument, treść nowego zadania,  dodaje nowe zadanie bez done ponieważ zadanie nie jest a początku nie jest wykonane i odpala funkcje render     */
-    tasks.push({
-      content: newTaskContent,
-    });
-
-    render();
-  };
+  const tasks = [];
 
   const removeTask = (taskIndex) => {
-    /* removeTask dostaje tylko index zadania i za pomocą metody splice usuwa zadanie i odpala funkcje render   */
     tasks.splice(taskIndex, 1);
     render();
   };
 
   const toggleTaskDone = (taskIndex) => {
-    /*toggleTaskDone dostaje tylko index zadania i pod konkretnym indexem zmienia wartość done i znowu odpala render  */
     tasks[taskIndex].done = !tasks[taskIndex].done;
     render();
   };
 
-  const bindEvents = () => {
-    /*funkcja bindevents bierze wszytskie przyciski do usuwania i za pomomoca querySellectorAll to jest node list później możemy iterować za pomocą forEach i dlatego możemy każdemu z tych przyciskó przypisać ecentlestener click i użyć funkcji remove task (podobnie dla toggleDoneButtons)   */
+  const addNewTask = (newTaskContent) => {
+    tasks.push({ content: newTaskContent });
+    render();
+  };
+
+  const bindRemoveEvents = () => {
     const removeButtons = document.querySelectorAll(".js-remove");
 
-    removeButtons.forEach((removeButton, index) => {
+    removeButtons.forEach((removeButton, taskIndex) => {
       removeButton.addEventListener("click", () => {
-        removeTask(index);
-      });
-    });
-
-    const toggleDoneButtons = document.querySelectorAll(".js-done");
-
-    toggleDoneButtons.forEach((toggleDoneButton, index) => {
-      toggleDoneButton.addEventListener("click", () => {
-        toggleTaskDone(index);
+        removeTask(taskIndex);
       });
     });
   };
 
-  /*funkcja render za każdym razem po jakiejkoklwiek akcji zupełnie od nowa renderuje zawartoś html tej listy (template strings) łącze html z jakimiś wyrażeniami i dlatego czy task jest done to dodaje stylowanie (przekreślenie lub nie)   */
+  const bindToggleDoneEvents = () => {
+    const toggleDoneButtons = document.querySelectorAll(".js-toggleDone");
+
+    toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(taskIndex);
+      });
+    });
+  };
+
   const render = () => {
-    let htmlString = "";
+    let tasksListHtmlContent = "";
 
     for (const task of tasks) {
-      htmlString += `
-        <li${task.done ? ' style="text-decoration: line-through"' : ""}>
-        <button class="js-done">zrobione?</button>
-        <button class="js-remove">usuń</button>
-          ${task.content}</li$>`;
+      tasksListHtmlContent += `
+        <li class="tasks__item js-task">
+     <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+     ${task.done ? "✔" : ""}
+     </button>
+     <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
+     ${task.content}
+     </span>
+     <button class="tasks__button tasks__button--remove js-remove">
+     ☒
+     </button>
+     </li>
+     `;
     }
 
-    document.querySelector(" .js-tasks").innerHTML = htmlString;
+    document.querySelector(".js-tasks").innerHTML = tasksListHtmlContent;
 
-    bindEvents();
+    bindRemoveEvents();
+    bindToggleDoneEvents();
   };
 
   const onFormSubmit = (event) => {
-    /*onFurmSubmit bierze treść co jest wpisana w formularzu (inpucie)  */
     event.preventDefault();
 
-    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    const newTaskElement = document.querySelector(".js-newTask");
+    const newTaskContent = newTaskElement.value.trim();
 
-    if (newTaskContent === "") {
-      /*jeżeli jest pusty formularz to nic nie robi  */
-      return;
+    if (newTaskContent !== "") {
+      addNewTask(newTaskContent);
+      newTaskElement.value = "";
     }
 
-    addNewTask(
-      newTaskContent
-    ); /*jeżeli jest coś wpisane dodaje nowe zadanie do listy  */
+    newTaskElement.focus();
   };
 
   const init = () => {
-    /*funkcja init początkowy render dzięki temu widzimy wpisaną treść na początku  */
     render();
 
-    const form = document.querySelector(
-      ".js-form"
-    ); /* do formularza dodajemy onform Submit*/
-
+    const form = document.querySelector(".js-form");
     form.addEventListener("submit", onFormSubmit);
   };
 
